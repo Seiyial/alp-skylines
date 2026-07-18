1d7a1d80c7121b800b57156f4ba864de9578d94f
Sayhao Lim <sayhao@alpinevalleysoftware.co>
2026-07-18 15:54:21 +0800

    fix prisma crash

--- backend/src/lib/db/prisma.ts
+++ backend/src/lib/db/prisma.ts
@@ -1,3 +1,11 @@
+import { env } from '@/env'
+import { PrismaPg } from '@prisma/adapter-pg'
+import 'dotenv/config'
 import { PrismaClient } from '../../generated/prisma'
 
-export const pris = new PrismaClient()
+const adapter = new PrismaPg({
+	connectionString: env.DATABASE_URL
+})
+export const pris = new PrismaClient({
+	adapter
+})
